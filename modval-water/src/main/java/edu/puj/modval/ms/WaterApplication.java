ea926f812343a5cdf33f63298246bf3b41fbc9d0
Michael Salgado <salgadosmichael@javeriana.edu.co>
2020-05-25 11:56:44 -0500

    se agrega ms como submodulo (water)

---
+++ modval-water/src/main/java/edu/puj/modval/ms/WaterApplication.java
@@ -0,0 +1,64 @@
+package edu.puj.modval.ms;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
+@EnableEurekaClient
+@SpringBootApplication
+public class WaterApplication {
+    private static final Logger log = LoggerFactory.getLogger(WaterApplication.class);
+
+    private final Environment env;
+
+    public WaterApplication(Environment env) {
+        this.env = env;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(WaterApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        logApplicationStartup(env);
+    }
+
+    private static void logApplicationStartup(Environment env) {
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isBlank(contextPath)) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}{}\n\t" +
+                        "External: \t{}://{}:{}{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.getActiveProfiles());
+    }
+
+}
