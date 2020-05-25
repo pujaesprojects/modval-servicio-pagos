a0dfed7dfe31f90fec9aff1d046f897c880ea6b9
Michael Salgado <elesdoar@gmail.com>
2020-05-24 19:17:33 -0500

    se agrega cliente eureka, para registry de servicios

--- src/main/java/edu/puj/modval/ms/AESApplication.java
+++ src/main/java/edu/puj/modval/ms/AESApplication.java
@@ -5,11 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class AESApplication {
     private static final Logger log = LoggerFactory.getLogger(AESApplication.class);
--- src/main/java/edu/puj/modval/ms/config/AESCorsConfiguration.java
+++ src/main/java/edu/puj/modval/ms/config/AESCorsConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class AESCorsConfiguration {
     @Bean
