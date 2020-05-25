275090d7d37b57b1cc8cfee333dd8bf85deb8d2c
Michael Salgado <salgadosmichael@javeriana.edu.co>
2020-05-25 14:40:51 -0500

    se crea servicio que obtiene los convenios

--- modval-gateway/src/main/java/edu/puj/modval/ms/controller/ServiceInstanceController.java
+++ modval-gateway/src/main/java/edu/puj/modval/ms/controller/ServiceInstanceController.java
@@ -2,11 +2,17 @@ package edu.puj.modval.ms.controller;
 
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class ServiceInstanceController {
@@ -18,7 +24,26 @@ public class ServiceInstanceController {
 
     @RequestMapping("/service-instances/{applicationName}")
     public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
-        this.discoveryClient.getServices();
         return this.discoveryClient.getInstances(applicationName);
+    }
+
+    @GetMapping(value = "/convenios", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, List<String>>> getServices() {
+        List<String> convenios = this.discoveryClient.getServices()
+                .stream()
+                .map(serviceId -> {
+                    List<ServiceInstance> serviceInstance = this.discoveryClient.getInstances(serviceId);
+                    if(serviceInstance.size() > 0 && serviceInstance.get(0).getMetadata().containsKey("convenio")) {
+                        return serviceInstance.get(0).getMetadata().get("convenio");
+                    };
+
+                    return null;
+                })
+                .filter(convenio -> convenio != null)
+                .collect(Collectors.toList());
+
+        Map<String, List<String>> response = new HashMap<>();
+        response.put("convenios", convenios);
+        return ResponseEntity.ok(response);
     }
 }
